013656cd7120d860c43d914ff0959ed25219a318
Uladzislau Miranchuk <umiranchuk@travix.com>
2019-02-11 19:31:48 +0300

    add splash-screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 import RootNavigation from './src/navigation/RootNavigation';
 import withInternetConnection from './src/hocs/withInternetConnection';
 
 const RootNavigationContainer = createAppContainer(RootNavigation);
 
-const App = () => (
-  <RootNavigationContainer />
-);
+class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return <RootNavigationContainer />;
+  }
+}
 
 export default withInternetConnection(App);
