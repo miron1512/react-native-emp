022d132bb971729708b4de1e119b7367298c0a78
Uladzislau Miranchuk <umiranchuk@travix.com>
2019-02-12 20:47:25 +0300

    move api calls to separate file

---
+++ src/api/constants.tsx
@@ -0,0 +1,6 @@
+export const BASE_URL = 'http://ecsc00a02fb3.epam.com';
+export const HEADERS = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+export const PAGE_SIZE = 15;
---
+++ src/api/index.tsx
@@ -0,0 +1,2 @@
+export { getToken, getProducts, addProductToCart, createCart } from './api';
+export { PAGE_SIZE } from './constants';
